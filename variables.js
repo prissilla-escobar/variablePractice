43d582362b809927f569c6c135b7c46a445b610c
Prissilla Escobar <prissilla988@gmail.com>
2023-04-18 18:49:36 -0600

    correct variable names and add comments sign

--- variables.js
+++ variables.js
@@ -1,11 +1,11 @@
-# Variables Practice
+// Variables Practice
 
-## Strings
-- var coordinates = "39.7392° N, 104.9903° W"
-- var phoneNumber = "1-800-123-4567"
-- var email = "test@test.com"
+// Strings
+var coordinates = "39.7392° N, 104.9903° W"
+var phoneNumber = "1-800-123-4567"
+var email = "test@test.com"
 
-## Numbers
+// Numbers
 
 
-## Strings
+// Strings
